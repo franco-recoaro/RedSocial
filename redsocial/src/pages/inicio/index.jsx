c5f5e0337583d79bc71ca0d49cc13b40c3fb6d65
Brian <beerre20@gmail.com>
2024-09-04 19:13:50 -0300

    Creando Main

---
+++ redsocial/src/pages/inicio/index.jsx
@@ -0,0 +1,14 @@
+import { NavBar } from "../../components";
+
+const Main = () => {
+  return (
+    <div>
+      <NavBar />
+      <div>
+        <h1>Inicio</h1>
+      </div>
+    </div>
+  );
+};
+
+export { Main };
