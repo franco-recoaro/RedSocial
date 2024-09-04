3af3679b95cd635dc0a756c95e9ce54acfa318d3
Brian <beerre20@gmail.com>
2024-09-04 18:37:33 -0300

    Instalando dependencias

--- redsocial/src/components/navbar/index.jsx
+++ redsocial/src/components/navbar/index.jsx
@@ -1,6 +1,9 @@
+import toallin from "../../resources/images/toallin.png"
+import {Link} from "react-router-dom";
 const NavBar = () => {
     return (
-        <div>
+        <div className="navbarContainer">
+            <img src={toallin} alt="Logo" />
             <ul>
                 <li><a href="#">Inicio</a></li>
                 <li><a href="#">Perfil</a></li>
