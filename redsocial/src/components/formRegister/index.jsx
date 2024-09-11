40b6b550863c5c132ed7f135eb4de8bd8b942d9f
Brian <beerre20@gmail.com>
2024-09-10 21:12:41 -0300

    Creando almacenamiento datos registro

--- redsocial/src/components/formRegister/index.jsx
+++ redsocial/src/components/formRegister/index.jsx
@@ -2,40 +2,103 @@ import { Link } from "react-router-dom";
 import { useState } from "react";
 
 const RegisterForm = () => {
-    const [datos, setDatos] = useState({ name: "", apellido: "", email: "", ubicacion: "", sexo: "", fechaNac: "", username: "", password: "", fotoPerfil: "" })
-    return (
-        <div>
-            <form>
-                <label htmlFor="nombre">Nombre:</label>
-                <input id="nombre" name="nombre" type="text"
+  const [datos, setDatos] = useState({
+    nombre: "",
+    apellido: "",
+    email: "",
+    ubicacion: "",
+    sexo: "",
+    fechaNac: "",
+    username: "",
+    password: "",
+    fotoPerfil: "",
+  });
 
-                    onChange={(e) => { setDatos((prev) => ({ ...prev, nombre: e.target.value })) }
-                    } />
-                <label htmlFor="apellido">Apellido:</label>
-                <input id="apellido" name="apellido" type="text" />
-                <label htmlFor="email">Email:</label>
-                <input id="email" name="email" type="email" />
-                <label htmlFor="ubicacion">Ubicacion:</label>
-                <input id="ubicacion" name="ubicacion" type="text" />
-                <select name="sexo" id="sexo" defaultValue="">
-                    <option value="" disabled>-- Seleccione el sexo --</option>
-                    <option value="femenino">Femenino</option>
-                    <option value="masculino">Masculino</option>
-                    <option value="otros">Otros</option>
-                </select>
-                <label htmlFor="fechaNac">Fecha nacimiento:</label>
-                <input id="fechaNac" name="fechaNac" type="date" />
-                <label htmlFor="usernameRegister">Username:</label>
-                <input id="usernameRegister" name="username" type="text" />
-                <label htmlFor="password">Contraseña:</label>
-                <input id="password" name="password" type="text" />
-                <label htmlFor="fotoPerfil">Foto perfil:</label>
-                <input id="fotoPerfil" name="fotoPerfil" accept="image/*" type="file" />
-                <input type="submit" />
-            </form>
-            Si ya tienes cuenta, puedes iniciar sesión <Link to="/login">ahora mismo</Link>
-        </div>
-    );
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setDatos((prev) => {
+      return { ...prev, [name]: value };
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(datos);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="nombre">Nombre:</label>
+        <input
+          id="nombre"
+          name="nombre"
+          type="text"
+          onInput={handleInputChange}
+        />
+        <label htmlFor="apellido">Apellido:</label>
+        <input
+          id="apellido"
+          name="apellido"
+          type="text"
+          onInput={handleInputChange}
+        />
+        <label htmlFor="email">Email:</label>
+        <input
+          id="email"
+          name="email"
+          type="email"
+          onInput={handleInputChange}
+        />
+        <label htmlFor="ubicacion">Ubicacion:</label>
+        <input
+          id="ubicacion"
+          name="ubicacion"
+          type="text"
+          onInput={handleInputChange}
+        />
+        <select
+          name="sexo"
+          id="sexo"
+          defaultValue=""
+          onInput={handleInputChange}
+        >
+          <option value="" disabled>
+            -- Seleccione el sexo --
+          </option>
+          <option value="femenino">Femenino</option>
+          <option value="masculino">Masculino</option>
+          <option value="otros">Otros</option>
+        </select>
+        <label htmlFor="fechaNac">Fecha nacimiento:</label>
+        <input
+          id="fechaNac"
+          name="fechaNac"
+          type="date"
+          onInput={handleInputChange}
+        />
+        <label htmlFor="usernameRegister">Username:</label>
+        <input
+          id="usernameRegister"
+          name="username"
+          type="text"
+          onInput={handleInputChange}
+        />
+        <label htmlFor="password">Contraseña:</label>
+        <input
+          id="password"
+          name="password"
+          type="password"
+          onInput={handleInputChange}
+        />
+        <label htmlFor="fotoPerfil">Foto perfil:</label>
+        <input id="fotoPerfil" name="fotoPerfil" accept="image/*" type="file" />
+        <input type="submit" />
+      </form>
+      Si ya tienes cuenta, puedes iniciar sesión{" "}
+      <Link to="/login">ahora mismo</Link>
+    </div>
+  );
 };
 
 export { RegisterForm };
