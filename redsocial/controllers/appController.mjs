d5002baaf334bdfaf2c357238842d0f5063d9f35
Brian <beerre20@gmail.com>
2024-09-18 19:05:13 -0300

    Creando rutas, controllers, modules basicos

---
+++ redsocial/controllers/appController.mjs
@@ -0,0 +1,26 @@
+import connection from "../modules/conexion.mjs"; // VARIABLE CONEXION BASE DE DATOS ( IMPORTADA DE MODULES )
+
+// ACA SE CREAN LAS FUNCIONES QUE MANEJAN LAS RESPUESTAS REQ / RES  ( CONEXION BASE DE DATOS - SERVER - CLIENT)
+function traerTodos(req, res) {
+  // REQ = INFORMACION SOLICITADA POR EL FRONT
+  // RES = INFORMACION DE RESPUESTA DADA POR EL BACK
+  let query = "SELECT....."; // QUERY QUE SE VA A EJECUTAR A LA BASE DE DATOS
+
+  let resultados = connection.query(query, (err, results) => {
+    // METODO DE CONNECTION (CONEXION BASE DE DATOS ), PARA PODER ENVIARLE LA QUERY Y OBTENER UNA RESPUESTA PARA RESOLVER SOLICITUD
+    if (err) {
+      // SI LA LLAMADA A LA BASE DA ERROR , SE EJECUTA LA SIGUIENTE LINEA ( EN ESTE CASO SE ENVIA RESPUESTA SERVIDOR 500 AL FRONT CON UN MENSJAE)
+      res.status(500).send("Error al traer datos");
+    } else {
+      // SI LA LLAMADA ESTA OK , DEVUELVE UN JSON CON LA INFO BUSCADA EN LA BASE DE DATOS
+      res.status(200).json(results);
+    }
+  });
+}
+
+// SE CREA UN OBJETO PARA EXPORTAR TODOS LOS CONTROLADORES Y QUE SEA MAS FACIL SU MANENJO
+let appControllers = {
+  traerTodos,
+};
+
+export default appControllers;
---
+++ redsocial/modules/conexion.mjs
@@ -0,0 +1,12 @@
+import "dotenv/config";
+import mysql from "mysql2"; // LIBRERIA PARA USAR MYSQL
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT,
+});
+
+export default connection;
